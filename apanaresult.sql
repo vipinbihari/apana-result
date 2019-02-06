280fd33cee91a7b74ec8f81b31abd6831da4feec
VIPIN BIHARI <vipinbiharitiwari25@gmail.com>
2019-02-06 11:48:20 +0500

    adding apanaresult database sql schema

---
+++ apanaresult.sql
@@ -0,0 +1,171 @@
+-- MySQL dump 10.13  Distrib 5.7.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: apanaresult
+-- ------------------------------------------------------
+-- Server version	5.7.25-0ubuntu0.18.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admission`
+--
+
+DROP TABLE IF EXISTS `admission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admission` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `admitcard`
+--
+
+DROP TABLE IF EXISTS `admitcard`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admitcard` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `answerkey`
+--
+
+DROP TABLE IF EXISTS `answerkey`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `answerkey` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `certificate`
+--
+
+DROP TABLE IF EXISTS `certificate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `certificate` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `important`
+--
+
+DROP TABLE IF EXISTS `important`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `important` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `latestjobs`
+--
+
+DROP TABLE IF EXISTS `latestjobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `latestjobs` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `result`
+--
+
+DROP TABLE IF EXISTS `result`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `result` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `syllabus`
+--
+
+DROP TABLE IF EXISTS `syllabus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `syllabus` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `post_name` varchar(100) NOT NULL,
+  `generated_link` varchar(150) NOT NULL,
+  `sort_info` varchar(1000) DEFAULT NULL,
+  `img_thumb` varchar(150) DEFAULT NULL,
+  `dnd` binary(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-06 11:40:11
