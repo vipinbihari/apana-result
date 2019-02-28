104adef74585beb5a3fb7a7554545cd4730d7443
VIPIN BIHARI <vipinbiharitiwari25@gmail.com>
2019-02-28 19:36:36 +0530

    facebook metadata php file changement

--- mparts/facethumb.php
+++ mparts/facethumb.php
@@ -1,69 +1,55 @@
-<?php
-if(isset($_GET['type']) && isset($_GET['id'])){
-$type =$_GET['type'];
-$id = $_GET['id'];
-$query = "SELECT * FROM $type WHERE id=$id";
-$result = mysqli_query($conn, $query);
+<?php $conn = mysqli_connect('localhost', 'root', 'YES', 'apanaresult');
 
-while ($row = mysqli_fetch_assoc($result))
+if (!$conn)
 	{
-	 $q=$row['post_name'];
-    $id =$row['id'];
-    $link=$row['img_thumb'];
+	die("error connectig to the detabase" . mysqli_connect_error());
+	}
+?>
 
-if($link==NULL){
+<?php
 
-        $url = 'https://www.google.co.in/search?tbm=isch&q='. urlencode($q);
-
-
-
-// disguises the curl using fake headers and a fake user agent.
-function disguise_curl($url)
+if (isset($_GET['type']) && isset($_GET['id']))
 {
-  $curl = curl_init();
+	$type = $_GET['type'];
+	$id = $_GET['id'];
+	$query = "SELECT thumbnail_json, selected FROM thumbnails WHERE type_id=$id AND type='$type'";
+	$result = mysqli_query($conn, $query);
+	while ($row = mysqli_fetch_assoc($result))
+	{
+		$thumbnail_obj = json_decode($row['thumbnail_json']);
+		$selected = $row['selected'];
+	}
+	//var_dump($thumbnail_obj);
+if($thumbnail_obj != null){
+switch($selected){
+case 0:
+$thumbnail_link = $thumbnail_obj->default;
+break;
+case 1:
+$thumbnail_link = $thumbnail_obj->image1;
+break;
+case 2:
+$thumbnail_link = $thumbnail_obj->image2;
+break;
+case 3:
+$thumbnail_link = $thumbnail_obj->image3;
+break;
+case 4:
+$thumbnail_link = $thumbnail_obj->image4;
+break;
+default:
 
-  // Setup headers - I used the same headers from Firefox version 2.0.0.6
-  // below was split up because php.net said the line was too long. :/
-  $header[0] = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-  $header[] = "Cache-Control: max-age=0";
-  $header[] = "Connection: keep-alive";
-  $header[] = "Keep-Alive: 300";
-  $header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
-  $header[] = "Accept-Language: en-US,en;q=0.9,fr;q=0.8,hi;q=0.7,ru;q=0.6";
-//  $header[] = "Pragma: "; // browsers keep this blank.
-
-  curl_setopt($curl, CURLOPT_URL, $url);
-  curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36');
-  curl_setopt($curl, CURLOPT_HTTPHEADER, 'https://www.google.co.in/');
-  curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');
-  curl_setopt($curl, CURLOPT_AUTOREFERER, true);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-
-  $html = curl_exec($curl); // execute the curl command
-  return $html; // and finally, return $html
-}
-
-// uses the function and displays the text off the website
-$text = disguise_curl($url);
-//echo $text;
-$html1 = strstr($text, '"ou":"');
-$html2 = strstr($html1,'",', true);
-$link = strstr($html2,'http');
-//NULL condition ending down
-}
-//echo "<img src=$link>";
-
-
-$value = mysqli_real_escape_string($conn, $link);
-
-$query1 = "UPDATE $type SET img_thumb='$value' WHERE id='$id'";
-$ins = mysqli_query($conn, $query1);
-if($ins){
-//echo 'it was created ';
 }
 
 }
-//ifisset is ending down
+
+	$query = "SELECT post_name FROM $type WHERE id=$id";
+	$result = mysqli_query($conn,$query);
+	while($row = mysqli_fetch_assoc($result)){
+		$post_name = $row['post_name'];
+	}
 }
-                                                ?>
+
+//echo $post_name;
+//echo $thumbnail_link;
+?>
--- mparts/facethumbody.php
+++ mparts/facethumbody.php
@@ -1,5 +1,5 @@
-   
-<link itemprop="thumbnailUrl" href="<?php echo $link; ?>">
+
+<link itemprop="thumbnailUrl" href="<?php echo $thumbnail_link; ?>">
 <span itemprop="thumbnail" itemscope itemtype="http://schema.org/ImageObject">
-  <link itemprop="url" href="<?php echo $link; ?>">
+  <link itemprop="url" href="<?php echo $thumbnail_link; ?>">
 </span>
